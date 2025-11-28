2b9a4f842561ea198f5074c98249e795e2e8c137
IMAD <elbarkoukiimad1@gmail.com>
2025-11-28 10:43:41 +0100

    projet complet

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -9,6 +9,12 @@ func _ready():
 	else:
 		print("StartButton not found!")
 
+	var title_label = $TitleLabel
+	if title_label:
+		var tween = create_tween().set_loops()
+		tween.tween_property(title_label, "scale", Vector2(1.1, 1.1), 1.0).set_trans(Tween.TRANS_SINE)
+		tween.tween_property(title_label, "scale", Vector2(1.0, 1.0), 1.0).set_trans(Tween.TRANS_SINE)
+
 func _on_start_button_pressed():
 	# Change the scene to the main level
 	get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
